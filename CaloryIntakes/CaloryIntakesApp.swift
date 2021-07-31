c58bc75707bde0a6334e42187fef97fce5dab65b
saini1998 <sainiaaryaman1998@gmail.com>
2021-07-31 02:53:21 +0100

    Initial Commit

---
+++ CaloryIntakes/CaloryIntakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaloryIntakesApp.swift
+//  CaloryIntakes
+//
+//  Created by Aaryaman Saini on 31/07/21.
+//
+
+import SwiftUI
+
+@main
+struct CaloryIntakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CaloryIntakes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CaloryIntakes
+//
+//  Created by Aaryaman Saini on 31/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
